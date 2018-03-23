8c3ad51cdc088a0305037bbff9bf858df309a9a5
abdelmun3m <abdelmunemelbasiouny@gmail.com>
2018-03-23 16:04:35 +0200

    adding fire base Singleton Class in cluding firebase structure

---
+++ app/src/main/java/com/android/abdelmun3m/tripplaner/DBSingleTon.java
@@ -0,0 +1,63 @@
+package com.android.abdelmun3m.tripplaner; /**
+ * Created by abdelmun3m on 23/03/18.
+ */
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public final class DBSingleTon {
+
+        private static final String TAG = DBSingleTon.class.getSimpleName();
+        private static final String TRIP_KEY = "trips";
+        private static FirebaseDatabase DBinsatnce;
+        private static DatabaseReference DBreference;
+        private static ValueEventListener ValueListener;
+        //public FirebaseAuth myAuth;
+        //public FirebaseAuth.AuthStateListener myAuthListener;
+        //public FirebaseUser User ;
+
+        private  DBSingleTon(){
+            //DBinsatnce = FirebaseDatabase.getInstance(); // get an instance from the database .
+            //DBreference = DBinsatnce.getReference() ; // getting the reference to the root of the data base
+            //myAuth = FirebaseAuth.getInstance();
+        }
+
+        public static DatabaseReference GET_ROOT_DATABASE_REFERENCE(){
+
+            if(DBreference == null){
+                DBinsatnce = FirebaseDatabase.getInstance(); // get an instance from the database .
+                DBreference = DBinsatnce.getReference();
+            }
+            return DBreference;
+        }
+
+        public static DatabaseReference GET_TRIP_REFERENCE(){
+
+            if(DBreference == null){
+                GET_ROOT_DATABASE_REFERENCE();
+            }
+            return DBreference.child(TRIP_KEY);
+        }
+
+//    public MainActivity.test getObstacle(){
+//
+//        ValueEventListener ObstacleListener = new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        };
+//
+//        tripReference.addValueEventListener(ObstacleListener);
+//        return  null;
+//    }
+
+
+    // to add record                 tripReference.push().setValue(new test());
+
+}
--- app/src/main/java/com/android/abdelmun3m/tripplaner/MainActivity.java
+++ app/src/main/java/com/android/abdelmun3m/tripplaner/MainActivity.java
@@ -2,12 +2,25 @@ package com.android.abdelmun3m.tripplaner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
 }
